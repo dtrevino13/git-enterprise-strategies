bdbaa107768e8a82bff956942cf1168353283da5
dtrevino13 <d.trevino@ufl.edu>
2023-01-29 13:11:38 -0500

    Merge changes from main

--- src/App.js
+++ src/App.js
@@ -5,16 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World! This is our team project.
         </p>	
-=======
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Daniela Trevino and this is my project </p>
->>>>>>> 94887db4474996d07b3011d397240c8d2f90c10c
       </header>
     </div>
   );
